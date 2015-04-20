c5a4608a311d50c133ee3b682cd037149951872a
Ji-sub Chung <jisub.chung0@gmail.com>
2015-04-20 10:27:35 -0700

    test

--- src/sort/prog3.java
+++ src/sort/prog3.java
@@ -42,7 +42,7 @@ class ArraySorts {
 	 *     5			2ptr		rand	 500		Y
 	 *     6			book		rand	 1			N
 	 */
-
+		
 	//QUICKSORT DRIVER METHODS
 	public static void QuickSort1(int[] a, int n) {
 		QuickSort1(a, 0, n-1);
@@ -53,16 +53,16 @@ class ArraySorts {
 		QuickSort2(a, 0, n-1);
 		InsertionSort(a, n);		
 	}
-
+	
 	public static void QuickSort3(int[] a, int n) {
 		QuickSort3(a, 0, n-1);	
 		InsertionSort(a, n);
 	}
-
+	
 	public static void QuickSort4(int[] a, int n) {
 		QuickSort4(a, 0, n-1);	
 	}
-
+	
 	public static void QuickSort5(int[] a, int n) {
 		QuickSort5(a, 0, n-1);
 		InsertionSort(a, n);		
@@ -71,9 +71,10 @@ class ArraySorts {
 	public static void QuickSort6(int[] a, int n) {
 		QuickSort6(a, 0, n-1);	
 	}
-
+	
 	//QUICKSORT RECURSIVE METHODS
 	private static void QuickSort1(int[] a, int left, int right) {
+<<<<<<< HEAD
 		int partitions[];
 		//[0] first index of partitioned pivot elements
 		//[1] last index of partitioned pivot elements
@@ -118,41 +119,35 @@ class ArraySorts {
 					right = partitions[1] - 1; // QS(a, lf, pivot - 1)
 				}
 			}*/
+=======
+		//easy case
+		if(((right+1)-left) <= 20) {
+		}
+		else {
+			int lastSmall = partitionBook(a,left,right,false);
+			QuickSort1(a, left, lastSmall-1);
+
+			//this could be optimized
+			QuickSort1(a, lastSmall+1, right);
+>>>>>>> parent of 08eb6b7... Finished
 		}
 	}
 
 	private static void QuickSort2(int[] a, int left, int right) {
-		/*/easy case
+		//easy case
 		if(((right+1)-left) <= 20) {
 		}
 		else {
 			int lastSmall = partition(a,left,right);
-			QuickSort2(a, left, lastSmall);
+			QuickSort1(a, left, lastSmall);
 
 			//this could be optimized
-			QuickSort2(a, lastSmall+1, right);
+			QuickSort1(a, lastSmall+1, right);
 		}
-		*/
-
-		
-		while (right - left>= 20) {
-		     int pivot = left + (int) (Math.random() * (right - left + 1));  // bonus hint for finding the random pivot...
-		     //  book's pivot method
-		     pivot = partition(a,left, right, pivot);
-		     // figure out which partition is smaller
-		     if ( (pivot - 1) - left < right - (pivot + 1) ) { // left side is smaller
-		          QuickSort2(a, left, pivot);
-		          // eliminate the tail recursion by setting the parameters accordingly
-		          left = pivot + 1;  // QS(a, pivot + 1, rt,)
-		     } else { // right side is smaller
-		          QuickSort2(a, pivot + 1, right);
-		          right = pivot; // QS(a, lf, pivot - 1)
-		     }
-		}
-		
 	}
-
+	
 	private static void QuickSort3(int[] a, int left, int right) {
+<<<<<<< HEAD
 		int partitions[];
 		//[0] first index of partitioned pivot elements
 		//[1] last index of partitioned pivot elements
@@ -196,6 +191,17 @@ class ArraySorts {
 					right = partitions[1] - 1; // QS(a, lf, pivot - 1)
 				}
 			}*/
+=======
+		//easy case
+		if(((right+1)-left) <= 20) {
+		}
+		else {
+			int lastSmall = partitionBook(a,left,right,true);
+			QuickSort1(a, left, lastSmall-1);
+
+			//this could be optimized
+			QuickSort1(a, lastSmall+1, right);
+>>>>>>> parent of 08eb6b7... Finished
 		}
 	}
 
@@ -204,55 +210,28 @@ class ArraySorts {
 		if(((right+1)-left) <= 1) {
 		}
 		else {
-			int pivot = left + (int) (Math.random() * (right - left + 1));
-			int lastSmall = partition(a,left,right, pivot);
-			QuickSort4(a, left, lastSmall);
+			int lastSmall = partition(a,left,right);
+			QuickSort1(a, left, lastSmall);
 
 			//this could be optimized
-			QuickSort4(a, lastSmall+1, right);
+			QuickSort1(a, lastSmall+1, right);
 		}
-		/*while (right - left>= 1) {
-		     int pivot = left + (int) (Math.random() * (right - left + 1));  // bonus hint for finding the random pivot...
-		     pivot = partition(a,left, right, pivot);
-		     // figure out which partition is smaller
-		     if ( (pivot - 1) - left < right - (pivot + 1) ) { // left side is smaller
-		          QuickSort4(a, left, pivot);
-		          // eliminate the tail recursion by setting the parameters accordingly
-		          left = pivot + 1;  // QS(a, pivot + 1, rt,)
-		     } else { // right side is smaller
-		          QuickSort4(a, pivot + 1, right);
-		          right = pivot; // QS(a, lf, pivot - 1)
-		     }
-		}*/
 	}
-
+	
 	private static void QuickSort5(int[] a, int left, int right) {
-		/*/easy case
+		//easy case
 		if(((right+1)-left) <= 500) {
 		}
 		else {
 			int lastSmall = partition(a,left,right);
-			QuickSort5(a, left, lastSmall);
+			QuickSort1(a, left, lastSmall);
 
 			//this could be optimized
-			QuickSort5(a, lastSmall+1, right);
-		}*/
-		while (right - left>= 500) {
-		     int pivot = left + (int) (Math.random() * (right - left + 1));  // bonus hint for finding the random pivot...
-		     //  book's pivot method
-		     pivot = partition(a,left, right, pivot);
-		     // figure out which partition is smaller
-		     if ( (pivot - 1) - left < right - (pivot + 1) ) { // left side is smaller
-		          QuickSort5(a, left, pivot);
-		          // eliminate the tail recursion by setting the parameters accordingly
-		          left = pivot + 1;  // QS(a, pivot + 1, rt,)
-		     } else { // right side is smaller
-		          QuickSort5(a, pivot + 1, right);
-		          right = pivot; // QS(a, lf, pivot - 1)
-		     }
+			QuickSort1(a, lastSmall+1, right);
 		}
 	}
 	
+<<<<<<< HEAD
 	//something
 	private static void QuickSort6(int[] a, int left, int right) {
 		int partitions[];
@@ -298,15 +277,28 @@ class ArraySorts {
 					right = partitions[1] - 1; // QS(a, lf, pivot - 1)
 				}
 			}*/
+=======
+	private static void QuickSort6(int[] a, int left, int right) {
+		//easy case
+		if(((right+1)-left) <= 1) {
+		}
+		else {
+			int lastSmall = partitionBook(a,left,right,false);
+			QuickSort1(a, left, lastSmall-1);
+
+			//this could be optimized
+			QuickSort1(a, lastSmall+1, right);
+>>>>>>> parent of 08eb6b7... Finished
 		}
 	}
 	
 	//QUICKSORT PARTITIONS
-	private static int[] partitionBook(int[] a, int start, int end, int pivot) {
+	private static int partitionBook(int[] a, int start, int end, boolean useLeft) {
 		//This partition will separate less than and greater than blocks according
 		// to a randomly selected partition value.
 		//The pivot is part of the less than block
 		//Returns index of the last less element (the pivot)
+<<<<<<< HEAD
 		
 		
 		int unknown = start;
@@ -317,6 +309,21 @@ class ArraySorts {
 		int temp = a[start];
 		a[start] = pivotValue;
 		a[pivot] = temp;
+=======
+		int unknown = start+1;
+		int lastSmall = start;
+		int holdElement = a[start];
+		int pivot;
+		if(!useLeft) {
+			//pick a random pivot and swap to front
+			Random r = new Random();
+			pivot = r.nextInt(end-start+1)+start;
+			a[start] = a[pivot];
+			a[pivot] = holdElement;
+		}
+		else {} //use left most element
+		pivot = a[start];
+>>>>>>> parent of 08eb6b7... Finished
 		//begin partitioning
 		while(unknown <= end) {
 			
@@ -371,6 +378,7 @@ class ArraySorts {
 				else {
 					temp = a[unknown];
 					lastSmall++;
+<<<<<<< HEAD
 					a[unknown] = a[lastSmall];
 					a[lastSmall] = temp;
 				}
@@ -403,12 +411,20 @@ class ArraySorts {
 				}
 				else {
 				}
+=======
+					holdElement = a[lastSmall];
+					a[lastSmall] = a[unknown];
+					a[unknown] = holdElement;
+				}				
+			}
+>>>>>>> parent of 08eb6b7... Finished
 			else {
 				//do nothing
 				//this is the case where unknown > pivot 
 			}
 			unknown++;
 		}
+<<<<<<< HEAD
 		//temp = a[lastSmall];
 		//a[lastSmall] = a[start];
 		//a[start] = temp;
@@ -422,25 +438,32 @@ class ArraySorts {
 //		}
 //		partitions[0]=partitions[1]-partitions[0];
 		return partitions;
+=======
+		holdElement = a[lastSmall];
+		a[lastSmall] = a[start];
+		a[start] = holdElement;
+		return lastSmall;
+>>>>>>> parent of 08eb6b7... Finished
 	}
-
-	private static int partition(int[] a, int start, int end, int pivot) {
+	
+	public static int partition(int[] a, int start, int end) {
 		//This partition will move pointers towards each other, swapping elements
 		// as needed according to randomly selected partition value.
 		//The pivot is always swapped
 		//Returns index of last less (may or may not include pivot)
 		int smaller = start;
 		int larger = end;
+		Random r = new Random();
+		int pivot = a[r.nextInt(end-start+1)+start];
 		int temp;
-		int piv = a[pivot];
 		while(smaller <= larger) {
-			if(a[smaller] < piv) {
+			if(a[smaller] < pivot) {
 				smaller++;
 			}
-			else if(a[larger] > piv) {
+			else if(a[larger] > pivot) {
 				larger--;
 			}
-			else if((a[smaller] >= piv) && (a[larger] <= piv)) {
+			else if((a[smaller] >= pivot) && (a[larger] <= pivot)) {
 				temp = a[smaller];
 				a[smaller] = a[larger];
 				a[larger] = temp;
@@ -452,65 +475,39 @@ class ArraySorts {
 		return larger;
 	}
 
-
+	
 	//BEGIN HEAPSORTS
-
-	//Sort elements by building a heap from the top
-	// (adding children then correcting)
-	//Then swapping the root (largest) with the last child then
-	// fixing the heap without the new last element (repeat)
+	
+	//TOP DOWN
 	public static void HeapSort1(int[] a, int n) {
+		//Shift, no swapping allowed
+		//reorganize data into a heap
 		buildHeapFromTop(a, n);
-		int endHeap = n-1;
+		//reorganize data in sorted order
+		int endHeap = n;
 		int temp;
 		while(endHeap > 0) {
-			temp = a[endHeap];
-			a[endHeap] = a[0];
-			a[0] = temp;
+			temp = a[n-endHeap];
+			for(int i = 0; i < endHeap; i++) {
+				a[i] = a[i+1];
+			}
+			a[endHeap] = temp;
 			endHeap--;
-			//trickleDown(a, 0, endHeap);
+			trickleUp(a, 0, endHeap);
 		}
 	}
-
-	//Sorts elements by building a heap from the bottom
-	// (adding parents then correcting)
-	//Then swapping the root (largest) with the last child then
-	// fixing the heap without the new last element (repeat)
-	public static void HeapSort2(int[] a, int n) {
-		buildHeapFromBottom(a, n);
-		int endHeap = n-1;
-		int temp;
-		while(endHeap > 0) {
-			temp = a[endHeap];
-			a[endHeap] = a[0];
-			a[0] = temp;
-			endHeap--;
-			//trickleDown(a, 0, endHeap);
-		}
-	}
-
+	
 	//start from the beginning of the heap (single root)
 	// then add more elements to the heap
-	private static void buildHeapFromTop(int[] a, int n) {
+	public static void buildHeapFromTop(int[] a, int n) {
 		int end = 1;
 		while (end < n) {
 			trickleUp(a, 0, end);
 			end++;
 		}
 	}
-
-	//Start from the end of the heap
-	//add parents going towards the root
-	private static void buildHeapFromBottom(int[] a, int n) {
-		int start = ((n-2)/2);
-
-		while(start >= 0) {
-			trickleDown(a, start, n-1);
-			start--;
-		}
-	}
-
-	//Trickle out of order children UP to their proper position
+	
+	//Move out of order elements UP to their proper position
 	private static void trickleUp(int[] a, int start, int endHeap) {
 		int temp;
 		int child = endHeap;
@@ -519,8 +516,6 @@ class ArraySorts {
 		while(!isHeap) {
 			if(a[parent] < a[child]) {
 				temp = a[child];
-				//so I don't quite understand how to shift vs swap here
-				/*
 				for(int i = child; i > parent;) {
 					if(i%2==0) {
 						a[i]=a[i-1];
@@ -531,8 +526,6 @@ class ArraySorts {
 						i=(i-1)/2;
 					}
 				}
-				*/
-				a[child] = a[parent];
 				a[parent] = temp;
 				if (parent != 0) {
 					child = parent;
@@ -545,40 +538,17 @@ class ArraySorts {
 		}
 	}
 	
-	//Trickle down out of order parents to proper place
-	private static void trickleDown(int[] a, int start, int endHeap) {
-		int root = start;
-		int temp;
-		int leftIndex = root*2+1;
-		int rightIndex = root*2+2;
-		int swapThis;
-		boolean isHeap = false;
-		while (leftIndex <= endHeap && !isHeap) {
-			swapThis = leftIndex;
-			//check if right node exists
-			if(rightIndex <= endHeap) {
-				//compare to see which of the two nodes is larger
-				if(a[rightIndex] > a[leftIndex]) {
-					//swap so that larger node is left
-					swapThis = rightIndex;
-				}
-			}
-			if(a[swapThis] > a[root]) {
-				//I can't figure out how to implement w/o swap
-				//please try to be gentle with ding'ing :(
-				temp = a[root];
-				a[root] = a[swapThis];				
-				a[swapThis] = temp;
-				root = swapThis;
-			}
-			else {
-				isHeap = true;
-			}
-			leftIndex = root*2+1;
-			rightIndex = root*2+2;
-		}
+	//Start from the end of the heap
+	//add elements going towards the front
+	public static void buildHeapFromBottom(int[] a, int n) {
+		
 	}
-
+	
+	//Move out of order elements DOWN to their proper position
+	private static void trickleDown(int[] a, int start, int endHeap) {
+		
+	}
+	
 	public static String myName() {
 		return "Ji-Sub Chung";
 	}
